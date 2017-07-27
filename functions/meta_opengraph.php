74de49e2883323bf5d2dd5b38d56a412294be9c8
brunodarshan <brunodarshan@gmail.com>
2017-07-27 12:40:50 -0300

    set image open graph

--- functions/meta_opengraph.php
+++ functions/meta_opengraph.php
@@ -4,7 +4,7 @@
  function open_graph_tags(){
     $infors = array(
       'title' => get_bloginfo('site')."". wp_title('-', false),
-      'image' => (has_post_thumbnail()) ? get_the_post_thumbnail_url() :  get_header_image(),
+      'image:url' => (has_post_thumbnail()) ? get_the_post_thumbnail_url() :  get_header_image(),
       'type' => 'blog',
       'url' => home_url(add_query_arg(array(),$wp->request)),
       'site_name' => get_bloginfo('name')
