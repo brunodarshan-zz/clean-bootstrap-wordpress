e79a2d75dd6448212cc9f1070eba774ea95c092e
brunodarshan <brunodarshan@gmail.com>
2017-07-27 10:21:22 -0300

    change position header

--- header.php
+++ header.php
@@ -30,6 +30,19 @@
         <?php endif; ?>
       <?php endif;
     } ?>
+    <header class="intro-header" style="background-image: url('<?php  set_src_image();?>');">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1> <?php is_home() ? bloginfo('name') : the_title(); ?></h1>
+                        <hr class="small">
+                        <!-- <span class="subheading"><?php bloginfo('description'); ?></span> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
     <nav  class="navbar navbar-default navbar-custom">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
 
-<header class="intro-header" style="background-image: url('<?php  set_src_image();?>');">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="site-heading">
-                    <h1> <?php is_home() ? bloginfo('name') : the_title(); ?></h1>
-                    <hr class="small">
-                    <!-- <span class="subheading"><?php bloginfo('description'); ?></span> -->
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
+
 <?php get_template_part('partial/postlist'); ?>
 <?php get_footer(); ?>
